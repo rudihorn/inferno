0ddc2a05ec234e872ef7ed8f9bb79f9f05c01fac
Jan Stolarek <jan.stolarek@ed.ac.uk>
2020-10-16 15:53:55 +0100

    Wibbles

--- src/Debug.ml
+++ src/Debug.ml
@@ -3,10 +3,12 @@ let enabled = false
 let print_ranks = true
 
 let print message =
-  if enabled then prerr_endline message; flush stderr
+  if enabled then prerr_endline message;
+  flush stderr
 
 let print_doc doc =
   if enabled then
-    PPrint.(ToChannel.pretty 0.9 80 stderr (doc ^^ hardline)); flush stderr
+    PPrint.(ToChannel.pretty 0.9 80 stderr (doc ^^ hardline));
+  flush stderr
 
 let fuel = 5
