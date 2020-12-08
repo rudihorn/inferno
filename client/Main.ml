08228c2f0161e23782dc54a43094f7f2340d30d1
Jan Stolarek <jan.stolarek@ed.ac.uk>
2020-12-08 12:22:38 +0000

    Debugging

--- client/Main.ml
+++ client/Main.ml
@@ -3,7 +3,7 @@ open F
 open Result
 
 let verbose =
-  false
+  true
 
 (* -------------------------------------------------------------------------- *)
 
@@ -1647,6 +1647,7 @@ let fml_e3_dot_no_lambda_sig =
   }
 
 let () =
+(*
   test env_test;
   (* PLDI paper examples *)
   test a1;
@@ -1746,5 +1747,7 @@ let () =
   test fml_mono_gen_test1;
   test fml_mono_gen_test2;
   test fml_e3_dot_no_lambda_sig
+*)
+  test fml_let_annot_8
 
 let () = print_summary_and_exit ()
